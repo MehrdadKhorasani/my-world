6abaa2ea74d0e9b687844e175039f1ff0ae0efb1
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-06-13 19:44:40 +0330

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void level_one(player_choose){
+    printf("\n\nKIR\n");
+}
+
+int main(){
+    const char *arr[] = {"your dad's gun", "your favorite book", "one of your friends"};
+    int choose;
+    // START OF THE GAME
+    printf("hello and welcome to my world.\n");
+    printf("before you start, I let you take one of your things and bring it to the game ... \n\n");
+    printf("what would you choose?\n");
+    for (int i = 0; i <= 2; i++){
+        printf("%d ", i);
+        puts(arr[i]);
+    };
+
+    scanf("%d", &choose);
+    printf("\nYou chose %s. I hope it will be handful.\n", arr[choose]);
+    printf("OK! Let's start the journey.");
+    level_one(choose);
+    return 0;
+}
