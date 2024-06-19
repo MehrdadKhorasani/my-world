cde792007653a6c1952558c89fc3a33aa393891a
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-06-19 23:32:59 +0330

    update first function

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-void level_one(player_choose){
-    printf("\nyour ");
-    puts(player_choose);
-    printf("lefts you... \n");
+void level_one(const char *player_choose){
+   if(strcmp(player_choose, "your dad's gun") == 0){
+        printf("1\n");
+    } else if(strcmp(player_choose, "your favorite book") == 0){
+        printf("2\n");
+    } else if(strcmp(player_choose, "one of your friends") == 0){
+        printf("3\n");
+    }
 }
 
 int main(){
@@ -14,7 +18,7 @@ int main(){
     printf("before you start, I let you take one of your things and bring it to the game ... \n\n");
     printf("what would you choose?\n");
     for (int i = 0; i <= 2; i++){
-        printf("%d ", i);
+        printf("%d ", i + 1);
         puts(arr[i]);
     };
 
